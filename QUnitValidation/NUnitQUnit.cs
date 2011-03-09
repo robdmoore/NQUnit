e348e1013b80f4c37fdbc32687b56cda282de5e6
Robert Moore <r.moore@curtin.edu.au>
2011-03-09 13:02:31 +0800

    Fixing problems with running the test using ReSharper test runner and refactored out NUnit code vs WatiN code

---
+++ QUnitValidation/NUnitQUnit.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace QUnitValidation
+{
+    public class NUnitQUnit : IDisposable
+    {
+        private readonly QUnitParser _qUnitParser;
+
+        public NUnitQUnit()
+        {
+            _qUnitParser = new QUnitParser();
+        }
+
+        public IEnumerable<QUnitTest> GetTests(params string[] filesToTest)
+        {
+            return filesToTest.SelectMany(file => _qUnitParser.GetQUnitTestResults(file)).ToArray();
+        }
+
+        public void Dispose()
+        {
+            _qUnitParser.Dispose();
+        }
+    }
+
+    public static class NUnitQUnitHelpers
+    {
+        public static void ShouldPass(this QUnitTest theTest)
+        {
+            Assert.That(theTest.Result.Split(' '), Has.Member("pass"), theTest.Message);
+        }
+    }
+}
