e3338a33e921e0df24d9e2ae6705d5ddccc99c1d
Robert Moore <me@robdmoore.id.au>
2011-03-11 00:40:06 +0800

    Adding sample web app with a dummy JavaScript library

---
+++ SampleWebApp/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace SampleWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ SampleWebApp/Scripts/my-javascript.js
@@ -0,0 +1,45 @@
+﻿/// <reference path="jquery-1.4.4-vsdoc.js" />
+var MyNamespace = (function ($) {
+
+	var that = {};
+
+	var counter = 0;
+
+	var multiply = function (y) {
+		if (!isNaN(y)) {
+			counter *= y;
+		}
+	};
+
+	var add = function (y) {
+		if (!isNaN(y)) {
+			counter += y * 1;
+		}
+	};
+
+	var increment = function () {
+		add(1);
+	};
+
+	var display = function () {
+		$("#display").text(counter);
+	};
+
+	var getCount = function () {
+		return counter;
+	};
+
+	var reset = function () {
+		counter = 0;
+	};
+
+	// Public methods
+	that.add = add;
+	that.multiply = multiply;
+	that.display = display;
+	that.increment = increment;
+	that.getCount = getCount;
+	that.reset = reset;
+
+	return that;
+})(jQuery);
