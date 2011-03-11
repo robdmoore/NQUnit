80124e75cfce9f3fbc522052c167af1df0704a50
Robert Moore <r.moore@curtin.edu.au>
2011-03-11 15:03:49 +0800

    Adding readme to give instructions for how to get up and running

--- NuGet/BuildNQUnit.NUnit.bat
+++ NuGet/BuildNQUnit.NUnit.bat
@@ -3,6 +3,7 @@ SET PROJECT=NQUnit.NUnit
 SET ASSEMBLYNAME=none
 REM Copy static content files
 xcopy /y/s/e "..\NQUnit.NUnit\JavaScriptTests" "NQUnit.NUnit\content\JavaScriptTests\"
+copy /y "nqunit.nunit.readme.txt" "NQUnit.NUnit\content\"
 del "NQUnit.NUnit\content\JavaScriptTests\test.html"
 del "NQUnit.NUnit\content\JavaScriptTests\tests.js"
 REM Build NQUnit.NUnit NuGet package
