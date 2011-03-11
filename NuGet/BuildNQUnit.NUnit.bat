95c0992165f2d2b9c635508127cd018edc712e66
Robert Moore <r.moore@curtin.edu.au>
2011-03-11 14:08:54 +0800

    Fixing up NQUnit.NUnit NuGet build file

--- NuGet/BuildNQUnit.NUnit.bat
+++ NuGet/BuildNQUnit.NUnit.bat
@@ -2,6 +2,8 @@ REM Set environment variables for NQUnit.NUnit
 SET PROJECT=NQUnit.NUnit
 SET ASSEMBLYNAME=none
 REM Copy static content files
-copy /y "..\NQUnit.NUnit\JavaScriptTests\" "NQUnit.NUnit\JavaScriptTests\"
+xcopy /y/s/e "..\NQUnit.NUnit\JavaScriptTests" "NQUnit.NUnit\JavaScriptTests\"
+del "NQUnit.NUnit\JavaScriptTests\test.html"
+del "NQUnit.NUnit\JavaScriptTests\tests.js"
 REM Build NQUnit.NUnit NuGet package
 rake
