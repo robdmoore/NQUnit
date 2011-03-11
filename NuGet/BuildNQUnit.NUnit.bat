a49895fd2454f8960d4b4e578964f19627c1ad2b
Robert Moore <r.moore@curtin.edu.au>
2011-03-11 14:50:05 +0800

    After reading NuGet spec it seems I need to have the content files in a content folder

--- NuGet/BuildNQUnit.NUnit.bat
+++ NuGet/BuildNQUnit.NUnit.bat
@@ -2,8 +2,8 @@ REM Set environment variables for NQUnit.NUnit
 SET PROJECT=NQUnit.NUnit
 SET ASSEMBLYNAME=none
 REM Copy static content files
-xcopy /y/s/e "..\NQUnit.NUnit\JavaScriptTests" "NQUnit.NUnit\JavaScriptTests\"
-del "NQUnit.NUnit\JavaScriptTests\test.html"
-del "NQUnit.NUnit\JavaScriptTests\tests.js"
+xcopy /y/s/e "..\NQUnit.NUnit\JavaScriptTests" "NQUnit.NUnit\content\JavaScriptTests\"
+del "NQUnit.NUnit\content\JavaScriptTests\test.html"
+del "NQUnit.NUnit\content\JavaScriptTests\tests.js"
 REM Build NQUnit.NUnit NuGet package
 rake
